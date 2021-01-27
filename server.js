0e3a6afc209a122c9cad88c5d26ade01fdd85c23
Edgar Talledos <onimenotsuki@gmail.com>
2021-01-27 14:33:49 -0600

    Agregada conexión con sequelize

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
 // Cargamos las variables de entorno
@@ -7,5 +8,22 @@ const app = require('./app');
 
 // Puerto por donde correrá el servidor
 const PORT = process.env.PORT || 3050;
+
+(async function testConnection() {
+  const sequelize = new Sequelize({
+    dialect: 'mariadb',
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 app.listen(PORT, () => console.log(`Servidor corriendo en localhost:${PORT}`));
