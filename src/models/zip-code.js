acbce84f0b10db208b7d2e94da78f25f8d06240c
Rola342 <rola_dimo@hotmail.com>
2021-01-28 15:52:57 -0600

    Correccion

--- src/models/zip-code.js
+++ src/models/zip-code.js
@@ -2,8 +2,8 @@ const { DataTypes } = require('sequelize');
 
 const sequelize = require('../../config/db');
 
-const zipCode = sequelize.define('zip-codes', {
-  keyZip: {
+const zipCode = sequelize.define('zip-code', {
+  keyCode: {
     type: DataTypes.INTEGER,
     unique: DataTypes.TRUE,
   },
