28bb35527ac7677f6b61c4a299ec98d4dfcbed5a
Edgar Talledos <onimenotsuki@gmail.com>
2021-01-29 12:55:36 -0600

    Agregado expansión de modelo population

--- src/controllers/zip-codes/get-one.js
+++ src/controllers/zip-codes/get-one.js
@@ -1,11 +1,14 @@
 const { StatusCodes } = require('http-status-codes');
 
-const { ZipCode } = require('../../models');
+const { ZipCode, Population } = require('../../models');
 
 module.exports = async ({ params: { zipCode } }, res) => {
   // TODO: Debe de estar implementada la búsqueda por :zipCode
   try {
-    const zipCodeRecord = await ZipCode.findOne({ where: { key: zipCode } });
+    const zipCodeRecord = await ZipCode.findOne({
+      where: { key: zipCode },
+      include: [Population],
+    });
 
     return res.status(StatusCodes.OK).json(zipCodeRecord);
   } catch (error) {
